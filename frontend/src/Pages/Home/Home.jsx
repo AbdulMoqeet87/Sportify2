0543b05d578f151729bad9899105bc77da84903a
AbdulMoqeet87 <abdulmoqeet87@gmail.com>
2024-05-16 07:34:22 +0500

    resolved

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider_ from '../../components/Slider'
 import NewRelease from '../../components/NewRelease'
 import NB from '../../components/Navbar'
-import { useLocation } from 'react-router-dom';
+
 
 const Home = () => {
   return (
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 import { FaCircleUser } from "react-icons/fa6";
 
